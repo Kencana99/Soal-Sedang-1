05e87dfff8bf6702013938df41f51dbc4ac76f11
Kencana99 <72028123+Kencana99@users.noreply.github.com>
2025-11-04 11:11:34 +0700

    Add files via upload

---
+++ Soal1Sedang.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Soal1Sedang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan bulat positif: ");
+        int angka = input.nextInt();
+
+        if (angka < 0) {
+            System.out.println("Bilangan harus positif!");
+        } else {
+            long faktorial = 1;
+
+            for (int i = 1; i <= angka; i++) {
+                faktorial *= i;
+            }
+
+            System.out.println("Bilangan : " + angka);
+            System.out.println("Faktorial: " + faktorial);
+        }
+
+        input.close();
+    }
+}
